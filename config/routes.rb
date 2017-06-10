9203289530f48bb960275f61bceda0c585d107a5
pemurashum <pei.shum1227@gmial.com>
2017-06-10 19:48:41 +0900

    add user db

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'user/index'
+  get 'user/index' => "user#index"
 
-  get 'home/top'
+  get 'home/top' => "home#top"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
